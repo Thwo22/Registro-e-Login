8e05a9c1355173007e5ba0dc500771503c777b64
Thiago Santos <137359517+Thwo22@users.noreply.github.com>
2025-01-21 16:00:38 -0300

    1.6.1

--- src/script.js
+++ src/script.js
@@ -1,18 +1,34 @@
 
-let email = document.getElementById(`gmail`).value;
-let password = document.getElementById(`senha`).value;
+let email, senha;
 
 function cadastrar() {
-    alert(`Sucesso!`);
-    location.href = `login.html`;
+    email = document.getElementById(`gmail`).value.trim();
+    senha = document.getElementById(`senha`).value.trim();
+
+    if (email && senha) {
+        alert(`Cadastro realizado com sucesso!`)
+        location.href = `login.html?email=${encodeURIComponent(email)}&senha=${encodeURIComponent(senha)}` 
+    }else {
+        alert(`por favor, preencha os campos corretamente!`)
+    }
 }
 
 function logar() {
-    let gmail = document.getElementById(`gmail2`).value;
-    let senha = document.getElementById(`senha2`).value;
+    let params = new URLSearchParams(window.location.search);
+    let email = params.get(`gmail`);
+    let senha = params.get(`senha`);
 
-    if (gmail2 === email && senha2 === password)  {
-        alert(`Bem-vindo ao Site!`);
+    let gmail2 = document.getElementById(`gmail2`).value.trim()
+    let senha2 = document.getElementById(`senha2`).value.trim()
+
+    if (!gmail2 || !senha2) {
+        alert(`preencha os campos`);
+        return
+    }
+
+    if (gmail2 === email && senha2 === senha)  {
+        alert(`Sucesso! Redirecionando...`)
+        location.href = `home.html`;
     } else {
         alert(`usuario ou senha estão incorretos!`)
     }
